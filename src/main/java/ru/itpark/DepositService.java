e8a5d8ccbd0c6ac3f0e9e32fd939cfe9efb739c5
stlkralexdemo <stlkralex@yandex.ru>
2018-12-08 00:29:39 +0300

    Исправление багов

--- src/main/java/ru/itpark/DepositService.java
+++ src/main/java/ru/itpark/DepositService.java
@@ -2,8 +2,7 @@ package ru.itpark;
 
 public class DepositService {
     public int calculateDeposit(int deposit, int percent, int date) {
-        int sum = (deposit * percent * date / 100) + deposit;
-        return sum;
+        return deposit * percent * date / 100 + deposit;
     }
 }
 
